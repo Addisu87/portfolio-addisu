9bc60cb11a9b0f5a8ddb3c0cf18946d20e0c414b
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-11-05 21:21:28 +0300

    Update the background-circles and add profile-pic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image';
+import ProfilePic from '../assets/images/Profile.jpg';
 
 type Props = {};
 
@@ -17,8 +19,18 @@ function Hero({}: Props) {
     delaySpeed: 2000
   });
   return (
-    <div>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src={ProfilePic}
+        alt="Profile picture"
+      />
+
+      <div className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+        <h2>Software Engineer</h2>
+      </div>
+
       <h1>
         <span>{text}</span>
         <Cursor cursorColor="#F7AB0A" />
