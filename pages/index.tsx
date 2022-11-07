ad965c44bc485d0d94e9e476fba1d55f8143b49f
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-11-07 17:40:29 +0300

    Add project sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import WorkExperience from '../components/WorkExperience';
 import Skills from '../components/Skills';
+import Projects from '../components/Projects';
 
 const Home: NextPage = () => {
   return (
@@ -27,11 +28,13 @@ const Home: NextPage = () => {
         <WorkExperience />
       </section>
 
-      <section id="skills" className="snap-center">
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
     </div>
