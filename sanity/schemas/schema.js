88c6aeae214f995eb97e763162ecc331c071da94
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-11-11 16:45:30 +0300

    Update the end-points for the others sections

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,13 +5,13 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
 import pageInfo from './pageInfo';
-import experience from './experience';
-import social from './social';
 import skill from './skill';
+import experience from './experience';
 import project from './project';
+import social from './social';
 
 export default createSchema({
   name: 'default',
 
-  types: schemaTypes.concat([pageInfo, skill, experience, social, project])
+  types: schemaTypes.concat([pageInfo, skill, experience, project, social])
 });
