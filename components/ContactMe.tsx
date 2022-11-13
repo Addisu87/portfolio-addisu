853aa5679a96c4f52646df7195d1d407f9e28ff8
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-11-13 22:53:27 +0300

    Update the contact page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -18,20 +18,20 @@ function ContactMe({}: Props) {
     window.location.href = `addisu.haile@yahoo.com?subject=${formData.subject}&body=HI, my name is ${formData.name}. ${formData.message} (${formData.email})`;
   };
   return (
-    <div className="flex relative flex-col text-center md:text-left xl:flex-row mx-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <div className="flex relative flex-col text-center md:text-left xl:flex-row mx-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-10 mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
 
-      <div className="flex flex-col space-y-10">
-        <h4 className="text-4xl font-semibold text-center">
+      <div className="flex flex-col space-y-5">
+        <h4 className="text-3xl font-semibold text-center">
           I have got just what you need.{''}
           <span className="decoration-[#F7AB0A]/50 underline">
             Let us Talk.
           </span>
         </h4>
 
-        <div className="space-y-10">
+        <div className="space-y-5">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
             <p className="text-2xl">+251912912144</p>
