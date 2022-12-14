dc5cc3f678581b4a21ac5a5e4827b64277116b5f
Addisu <addisuhaile87@gmail.com>
2022-12-14 18:09:25 +0300

    Update the contact page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -19,18 +19,18 @@ const ContactMe = ({}: Props) => {
     window.location.href = `addisu.haile@yahoo.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
   };
   return (
-    <div className="flex relative flex-col text-center md:text-left xl:flex-row mx-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-10 mx-auto items-center overflow-hidden">
-      <h3 className="absolute top-24 uppercase tracking-[16px] text-gray-500 text-2xl">
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
 
-      <div className="flex flex-col space-y-5 top-40">
+      <div className="flex flex-col space-y-4 top-80">
         <h4 className="text-2xl font-light text-center">
           Let us {''}
           <span className="decoration-[#F7AB0A]/50 underline">Talk.</span>
         </h4>
 
-        <div className="flex flex-col space-y-7 top-40">
+        <div className="space-y-4">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
             <p className="text-xl">+251-912912144</p>
@@ -49,9 +49,9 @@ const ContactMe = ({}: Props) => {
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-4 mx-auto"
+          className="flex flex-col space-y-2 mx-auto w-fit"
         >
-          <div className="flex space-x-4">
+          <div className="flex space-x-2">
             <input
               {...register('name')}
               placeholder="Name"
