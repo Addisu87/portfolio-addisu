2977abc62afe218742d017eb87ee5887b7ff5c7d
Addisu <addisuhaile87@gmail.com>
2025-04-09 10:04:58 +0300

    Resolve conflict

--- sanity.ts
+++ sanity.ts
@@ -2,19 +2,11 @@ import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
-<<<<<<< HEAD
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVersion: '2022-11-16',
-  useCdn: process.env.NODE_ENV === 'production'
-};
-=======
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
 	apiVersion: "2023-10-20", // Use current date
 	useCdn: process.env.NODE_ENV === "production",
 }
->>>>>>> dev
 
 // Set up the client for fetching data in the getProps page functions
 export const sanityClient = createClient(config)
