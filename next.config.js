331de79cf357b695673c6cb1e924f9d48df2d382
Addisu <addisuhaile87@gmail.com>
2025-04-09 10:15:31 +0300

    Update env

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ const nextConfig = {
 			(process.env.VERCEL_URL
 				? `https://${process.env.VERCEL_URL}`
 				: "http://localhost:3000"),
+		NEXT_PUBLIC_BASE_URL:
+			process.env.NEXT_PUBLIC_BASE_URL ||
+			(process.env.VERCEL_URL
+				? `https://${process.env.VERCEL_URL}`
+				: "http://localhost:3000"),
 	},
 }
 
