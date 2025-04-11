7cb7ad2627a61c1684caac92085db6cf4e45e847
Addisu <addisuhaile87@gmail.com>
2025-04-11 10:01:12 +0300

    Update skill animation

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,17 +10,21 @@ type Props = {
 
 const Skill = ({ skill, directionLeft }: Props) => {
 	return (
-		<div className="group relative flex cursor-pointer mx-auto">
+		<div className="group relative flex cursor-pointer">
 			<motion.img
-				initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
-				transition={{ duration: 1 }}
+				initial={{ x: directionLeft ? -60 : 60, opacity: 0 }}
+				transition={{
+					duration: 0.3,
+					ease: "easeInOut",
+				}}
 				whileInView={{ opacity: 1, x: 0 }}
-				className="rounded-full border border-gray-500 object-cover w-14 h-14 md:w-20 md:h-20 filter group-hover:grayscale transition duration-300 ease-in-out group-hover:animate-bounce"
+				viewport={{ once: true }}
+				className="rounded-full border border-gray-500 object-cover w-16 h-16 md:w-20 md:h-20  filter group-hover:grayscale transition duration-300 ease-in-out  group-active:grayscale"
 				src={urlFor(skill?.image).url()}
 				alt={skill.title}
 			/>
 
-			<div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white rounded-full z-0 w-14 h-14 md:w-20 md:h-20">
+			<div className="absolute opacity-0 group-hover:opacity-80 group-active:opacity-80 transition duration-300 ease-in-out group-hover:bg-white group-active:bg-white rounded-full z-0 w-16 h-16 md:w-20 md:h-20">
 				<div className="flex items-center justify-center h-full">
 					<p className="text-base md:text-xl font-bold text-black opacity-100">
 						{skill.progress}%
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,14 +9,14 @@ type Props = {
 
 const Skills = ({ skills }: Props) => {
 	return (
-		<div className="h-screen relative flex flex-col items-center justify-center text-center">
+		<div className="min-h-screen relative flex flex-col items-center justify-center text-center">
 			<h3 className="section-title">Skills</h3>
 
 			<h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-				Hover over a skill for current proficiency
+				Hover over or tap a skill for current proficiency
 			</h3>
 
-			<motion.div className="grid grid-cols-4 gap-2 md:gap-4 w-fit max-w-[600px] mt-20">
+			<motion.div className="grid grid-cols-3 sm:grid-cols-4 gap-3 md:gap-4 w-fit max-w-[90vw] md:max-w-[600px] mt-20 px-4">
 				{skills?.slice(0, skills.length / 2).map((skill) => (
 					<Skill key={skill._id} skill={skill} />
 				))}
