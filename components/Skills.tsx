f1d36885fb18b5a7f47ba7c33d5c8eb7fcaee0da
Addisu <addisuhaile87@gmail.com>
2025-04-11 10:38:06 +0300

    Change grid-4 for skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -16,7 +16,7 @@ const Skills = ({ skills }: Props) => {
 				Hover over or tap a skill for current proficiency
 			</h3>
 
-			<motion.div className="grid grid-cols-3 sm:grid-cols-4 gap-3 md:gap-4 w-fit max-w-[90vw] md:max-w-[600px] mt-20 px-4">
+			<motion.div className="grid grid-cols-4 gap-3 md:gap-4 w-fit max-w-[90vw] md:max-w-[600px] mt-20 px-4">
 				{skills?.slice(0, skills.length / 2).map((skill) => (
 					<Skill key={skill._id} skill={skill} />
 				))}
